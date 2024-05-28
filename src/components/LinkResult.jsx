5cb307504117ac7fa7b2971f663abb1c38cacdea
Abraham Jose <abrahamjose002@gmail.com>
2024-05-28 07:45:58 +0530

    copy to clipboard configured

---
+++ src/components/LinkResult.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import {CopyToClipboard} from 'react-copy-to-clipboard'
+
+function LinkResult() {
+  const[shortenLink,setShortenLink] = useState("Hello World")
+  const[copied,setCopied] = useState(false)
+  return (
+    <div className='result'>
+      <p>{shortenLink}</p>
+      <CopyToClipboard text={shortenLink} onCopy={()=>setCopied(true)}>
+      <button className={copied ? "copied" : ""}>Copy to Clipboard</button>
+      </CopyToClipboard>
+    </div>
+  )
+}
+
+export default LinkResult
